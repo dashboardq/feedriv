71ce07c12c6c27cea35ed858b5a0c54b5d4b2d8b
Anthony Graddy <anthony@locationq.com>
2024-01-22 03:22:56 +0000

    Adds share images

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -15,3 +15,17 @@
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#00a300">
         <meta name="theme-color" content="#ffffff">
+
+		<meta property="og:title" content="FeedRiv Feed Reader" />
+		<meta property="og:description" content="A simple feed reader that lets you tag and rate items." />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="https://www.feedriv.com" />
+		<meta property="og:image" itemprop="image" content="https://www.feedriv.com/assets/images/share_1200x630.png" />
+		<meta property="og:image:secure_url" itemprop="image" content="https://www.feedriv.com/assets/images/share_1200x630.png" />
+		<meta property="og:image:width" content="1200" />
+		<meta property="og:image:height" content="630" />
+
+		<link itemprop="thumbnailUrl" href="https://www.feedriv.com/assets/images/share_1200x630.png"> 
+
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:image" content="https://www.feedriv.com/assets/images/share_1200x600.png">
